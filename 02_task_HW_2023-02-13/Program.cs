04a29802e4be985b82b3d7629bd1cfeacce2b895
Andrey_Shinkarenko <molzhaninriker@mail.ru>
2023-02-16 22:41:56 +0300

    Задать двумерный массив следующим правилом: Aₘₙ = m+n

---
+++ 02_task_HW_2023-02-13/Program.cs
@@ -0,0 +1,46 @@
+﻿// 02_task_HW_2023-02-13
+// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+Console.Clear();
+Console.WriteLine("Задать двумерный массив следующим правилом: A(m,n) = m+n" !);
+Console.WriteLine();
+
+void PrintMass(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i , j]} \t" !);
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateMass(int sizeM, int sizeN )
+{   
+    int[,] massive = new int[sizeM, sizeN];
+    Random rnd = new Random();
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            massive[i , j] = i + j;
+        }
+    }
+    return massive;
+}
+
+int ReadNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+
+int sizeM = ReadNum("Enter massive size m:"!);
+int sizeN = ReadNum("Enter massive size n:"!);
+int[,] array = CreateMass(sizeM, sizeN);
+
+Console.WriteLine($"Двумерный массив размером {sizeM} X {sizeN} заданный следующим правилом: A(m,n) = m+n:");
+PrintMass(array);
+Console.WriteLine();
